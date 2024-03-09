c3013ba218d3f7ac32a1a56b539e2c633283ca3f
Mauricio_del_Valle <mauriciodelvallefarrera@gmail.com>
2024-03-08 18:37:47 -0600

    Cambiando AppCompatActivity -> Activity en Consultar y Nuevo

--- app/src/main/java/com/example/ejemploretrofit/Consultar.java
+++ app/src/main/java/com/example/ejemploretrofit/Consultar.java
@@ -2,9 +2,10 @@ package com.example.ejemploretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Consultar extends AppCompatActivity {
+public class Consultar extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/ejemploretrofit/Nuevo.java
+++ app/src/main/java/com/example/ejemploretrofit/Nuevo.java
@@ -2,9 +2,10 @@ package com.example.ejemploretrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Nuevo extends AppCompatActivity {
+public class Nuevo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
