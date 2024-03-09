11495a6c8c98ea2f99b7dcffaa5b94cc6356f194
Mauricio_del_Valle <mauriciodelvallefarrera@gmail.com>
2024-03-08 18:33:25 -0600

    Metodos botones Nuevo y Consultar en MaintActivity

--- app/src/main/java/com/example/ejemploretrofit/MainActivity.java
+++ app/src/main/java/com/example/ejemploretrofit/MainActivity.java
@@ -3,13 +3,38 @@ package com.example.ejemploretrofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button btnConsultar;
+    Button btnNuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnConsultar = findViewById(R.id.btnConsultar);
+        btnNuevo = findViewById(R.id.btnNuevo);
+
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Consultar.class);
+                startActivity(i);
+            }
+        });
+
+        btnNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Nuevo.class);
+                startActivity(i);
+            }
+        });
     }
 }
